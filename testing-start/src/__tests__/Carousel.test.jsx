d3d9f30ec771b6b98266b2e02f75be55dde6f057
stephen.oconnor <stephen.oconnor@aryza.com>
2023-05-07 08:51:53 +0100

    testing setup, mock fetch, test hooks

---
+++ testing-start/src/__tests__/Carousel.test.jsx
@@ -0,0 +1,27 @@
+import { expect, test } from "vitest";
+import { render } from "@testing-library/react";
+
+import Carousel from "../Carousel";
+
+test("lets user click on thumbnails to make them hero", async () => {
+  const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+
+  const carousel = render(<Carousel images={images} />);
+
+  const hero = await carousel.findByTestId("hero");
+
+  expect(hero.src).toContain(images[0]);
+
+  for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+
+    const thumb = await carousel.findByTestId(`thumbnail${i}`);
+
+    await thumb.click();
+
+    expect(hero.src).toContain(image);
+    expect(Array.from(thumb.classList)).toContain("active");
+  }
+
+  carousel.unmount();
+});
